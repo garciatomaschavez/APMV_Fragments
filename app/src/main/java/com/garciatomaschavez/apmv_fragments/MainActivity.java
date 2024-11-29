ffb998b5c9cc59ebf2518cba592e8bffc970901f
Min Wang <minwang807@gmail.com>
2024-11-29 14:02:03 +0100

    fragment tierra By Min

--- app/src/main/java/com/garciatomaschavez/apmv_fragments/MainActivity.java
+++ app/src/main/java/com/garciatomaschavez/apmv_fragments/MainActivity.java
@@ -1,6 +1,7 @@
 package com.garciatomaschavez.apmv_fragments;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button abrirTierra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        abrirTierra = findViewById(R.id.openTierra);
+        abrirTierra.setOnClickListener(v -> {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main, new FragmentTierra())
+                            .addToBackStack(null)
+                            .commit();
+                });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
